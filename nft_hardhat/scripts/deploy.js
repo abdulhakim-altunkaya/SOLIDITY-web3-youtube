7ebcb24abe4e5482cc9bbd239419c8be635eaa7c
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-07-24 20:30:46 +0300

    nft hardhat project for youtube is pushed to github.

---
+++ nft_hardhat/scripts/deploy.js
@@ -0,0 +1,23 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const Goat = await hre.ethers.getContractFactory("Goat");
+  const goat = await Goat.deploy();
+
+  await goat.deployed();
+
+  console.log("Goat Contract is deployed to:", goat.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
